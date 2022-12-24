2572b56d779c1023bc1ae6dcb2d067bfdb791bf2
AnshVM <anshmalik2002@gmail.com>
2022-12-25 01:18:31 +0530

    draw paddles and ball

---
+++ pong-frontend/src/game.config.ts
@@ -0,0 +1,7 @@
+export const paddle_default = {
+    color:"white",
+    width:10,
+    height:30,
+    x:10,
+    y:10
+}
--- pong-frontend/src/main.ts
+++ pong-frontend/src/main.ts
@@ -1,23 +1,65 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import { paddle_default } from "./game.config";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+ctx.canvas.height = window.innerHeight;
+ctx.canvas.width = window.innerWidth;
+
+ctx.fillStyle = 'white';
+
+type Paddle = {
+  x:number,
+  y:number,
+  width:number,
+  height:number,
+}
+
+// type Ball = {
+//   x:number,
+//   y:number,
+//   radius:number,
+// }
+
+
+const drawRightPaddle = (ctx: CanvasRenderingContext2D,paddle:Paddle) => {
+  ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+}
+
+const drawLeftPaddle = (ctx: CanvasRenderingContext2D,paddle:Paddle) => {
+  ctx.fillRect(ctx.canvas.width - paddle.x - paddle.width, paddle.y, paddle.width, paddle.height)
+}
+
+const drawBall = (ctx: CanvasRenderingContext2D) => {
+  ctx.beginPath();
+  ctx.arc(ctx.canvas.width / 2, 30, 15, 0, Math.PI * 2, true)
+  ctx.fill();
+}
+
+drawRightPaddle(ctx,paddle_default);
+drawLeftPaddle(ctx,paddle_default);
+drawBall(ctx);
+
+const keyCodes = {
+  arrowUp:'ArrowUp',
+  arrowDown:'ArrowDown',
+  w:'KeyW',
+  s:'KeyS',
+}
+
+window.addEventListener('keydown',(event:KeyboardEvent) => {
+  if(event.code === keyCodes.arrowUp){
+    console.log('UP')
+  }
+  if(event.code === keyCodes.arrowDown) {
+    console.log('DOWN');
+  }
+  if(event.code === keyCodes.s){
+    console.log('S');
+  }
+  if(event.code === keyCodes.w){
+    console.log('W');
+  }
+})
+
+export { };
